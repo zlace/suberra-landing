910a20124452a424845fe9b3a25a3f4ec2b25fc2
zlace <zlace@hey.com>
2021-05-15 02:04:15 +0800

    feat: added landing page

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <img src="/suberra_dark.png" className="brand-home-logo" />
+      <h1>Seamless Crypto Payments</h1>
+      <p>Stay tuned.</p>
+    </main>
+  );
+}
+
+export default App;
